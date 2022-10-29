80c18596a1b7463d8da3a21197f4c1f4bbd05edc
chigosix <tngusrldnjs1@gmail.com>
2022-10-29 17:01:30 +0900

    refactor: 웹폰트 -> 시스템 폰트 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import "public/asset/fonts/font.css";
 import "styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "components/layout/Layout";
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,12 +5,6 @@ const Document = () => {
     <Html lang="ko">
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="stylesheet"
-          as="style"
-          crossOrigin="anonymous"
-          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.5/dist/web/static/pretendard.css"
-        />
       </Head>
       <body>
         <Main />
